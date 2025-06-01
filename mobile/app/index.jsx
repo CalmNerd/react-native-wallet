0b07dc21f9c12a6d0421d8bb8138ea3933275155
Mohit Kumar <mohittjee@gmail.com>
2025-06-01 16:07:54 +0530

    setting up styles and images

--- mobile/app/index.jsx
+++ mobile/app/index.jsx
@@ -1,6 +1,5 @@
 import { Link } from "expo-router";
-import { StyleSheet, Text, View } from "react-native";
-import { Image } from "expo-image";
+import { View } from "react-native";
 
 export default function Index() {
   return (
